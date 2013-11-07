7724d3633ccfd4022b68115f63a2de90a09a88b3
David Monaghan <github@allstruck.com>
2013-11-07 04:34:42 -0700

    Better grid, using buttons, 4x4 fluid

--- script/app.js
+++ script/app.js
@@ -5,6 +5,9 @@ $(document).ready(function ($) {
     sound[3] = new Audio('audio/3.mp3');
     sound[4] = new Audio('audio/4.mp3');
     sound[5] = new Audio('audio/5.mp3');
+    sound[6] = new Audio('audio/6.mp3');
+    sound[7] = new Audio('audio/7.mp3');
+    sound[8] = new Audio('audio/8.mp3');
 
     $(document).foundation();
     $('.hitpad').click(function() {
